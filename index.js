f7c4ca73c61bbaa3754fb73aa2ff713808b7127f
brightkingpin <luvandabright781@gmail.com>
2023-03-11 19:29:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Your code here
+// Adding moveDodgerLeft function
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  //adding moveDodgerRight function
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left + 1}px`;
+    }
+  });
+  
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  document.addEventListener('keydown', function(e) {
+    if ( e.key === "ArrowLeft" ) {
+      moveDodgerLeft();
+    }
+    if ( e.key === "ArrowRight" ) {
+      moveDodgerRight();
+    }
+  });
+  
